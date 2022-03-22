b49758336906c89e435803bbc6815c293f01016b
Santhos <santhoshsunthar@gmail.com>
2022-03-22 18:36:00 +0530

    data fetching and decryption ui

--- client/src/Decryption.jsx
+++ client/src/Decryption.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Data from './Data';
+import Axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function Decryption() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        Axios.get('http://localhost:3001/getall').then(
+            res => {
+                setData(res.data);
+            }
+        ).catch(error => {
+            console.log(error);
+        });
+    }, []);
+
     return (
         <div className='container'>
             <div className="decryption">
-                
+                <Data info={data}/>
             </div>
 
             <div className="nav">
--- server/index.js
+++ server/index.js
@@ -19,6 +19,31 @@ const db = mysql.createConnection({
     database:'passwords',
 });
 
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE IF NOT EXISTS passwords';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Database created...');
+    });
+});
+
+app.get('/createtable', (req, res) => {
+    let sql = 'CREATE TABLE IF NOT EXISTS mytable(password VARCHAR(255), title VARCHAR(255), iv VARCHAR(255))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('table created...');
+    });
+});
+
 app.post('/addpassword', (req, res) => {
      const {account, password} = req.body;
 
@@ -33,6 +58,15 @@ app.post('/addpassword', (req, res) => {
              res.send("success");
          }
      });
+});
+
+app.get('/getall', (req, res) => {
+    let sql = 'SELECT password, title FROM mytable';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
 });
 
 app.listen(port, () => {
