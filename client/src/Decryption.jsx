8feff06a7b8f92ffbc07feaa04237bfe52c546a0
Santhos <santhoshsunthar@gmail.com>
2022-03-23 19:18:00 +0530

    implementation changes

--- client/src/Decryption.jsx
+++ client/src/Decryption.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Data from './Data';
 import Axios from 'axios';
 import { useState, useEffect } from 'react';
+import DecrypterView from './DecrypterView'
 
-function Decryption() {
+function Decryption(props) {
     const [data, setData] = useState([]);
-
+    const [modal, setModal] = useState(false);
+    
     useEffect(() => {
         Axios.get('http://localhost:3001/getall').then(
             res => {
@@ -17,16 +18,34 @@ function Decryption() {
         });
     }, []);
 
+    const toggleModal = () => {
+        setModal(!modal);
+    }
+
     return (
-        <div className='container'>
-            <div className="decryption">
-                <Data info={data}/>
+        <>
+            <div className='container'>
+                <div className="decryption">
+                    {data.map(item => {
+                        return (<div className='data-view' onClick={toggleModal}>
+                            <h2 className="item-header">{item.title}</h2>
+                            <p className="item">{item.password}</p></div>);
+                    })}
+                </div>
+
+                <div className="nav">
+                    <Link to="/"><p>Encryption</p></Link>
+                </div>   
             </div>
 
-            <div className="nav">
-                <Link to="/"><p>Encryption</p></Link>
-            </div>   
-        </div>
+            {modal && (<div className="modal">
+                <div className="overlay">
+                    <DecrypterView/>
+                </div>     
+            </div>)}
+        </>
+        
+        
     )
 }
 
---
+++ client/src/modal/Modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Modal() {
+    const [modal, setModal] = useState(false);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Modal
